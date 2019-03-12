ba2bbd329cb48444f00ec7b7a89eaa922da2638c
Christopher Wilson <clwilson181@gmail.com>
2019-03-11 20:22:49 -0400

    Refactored for readability

--- word_count.py
+++ word_count.py
@@ -1,40 +1,59 @@
 """Counts and ranks the most common words in literary classics"""
-import numpy
-from collections import OrderedDict
+
 import time
 import re
 import json
+
+def update_json(results_dict):
+    """Adds the input dictionary to a json file"""
+    try:
+        with open("cache.json",'w',encoding='UTF-8') as file:
+            json.dump(results_dict,file)
+    except IOError:
+        pass
+
+def reorder_and_sort(input_dict):
+    """Reorders the input dictionary as a list of tulples and sorts it in decreasing order"""
+    unsorted_results = ((value, key) for (key,value) in input_dict.items())
+    return sorted(unsorted_results,reverse=True)
+
+def count_words(file_names):
+    word_counts = {}
+    for file_name in file_names:    
+        try:
+            with open(file_name, encoding='UTF-8') as file:
+                contents = file.read()
+        except IOError:
+            print('Error opening file')
+            continue
+        else:
+            word_list = set(contents.split())
+     
+        for word in word_list:
+            number_appearances = contents.count(word)
+
+            formatted_word =  re.sub("[^a-zA-Z0-9']", '', word.lower())
+
+            if formatted_word in word_counts.keys():
+                word_counts[formatted_word] += number_appearances
+            else:
+                word_counts[formatted_word] = number_appearances
+    return word_counts
+
 start_time = time.time()
+
 file_names = ['Books/a_modest_proposal.txt','Books/a_tale_of_two_cities.txt',
             'Books/dracula.txt','Books/frankenstein.txt','Books/heart_of_darkness.txt',
             'Books/little_women.txt','Books/moby_dick.txt','Books/oliver_twist.txt',
             'Books/pride_and_prejudice.txt','Books/the_scarlet_letter.txt','Books/ulysses.txt']
+ 
+word_counts = count_words(file_names)
 
-word_counts = OrderedDict()            
-for file_name in file_names:    
-    try:
-        with open(file_name, encoding='UTF-8') as file:
-            contents = file.read()
-            word_list = set(contents.split())
+sorted_results = reorder_and_sort(word_counts)
 
-            for word in word_list:
-                number_appearances = contents.count(word)
-                
-                if re.sub("[^a-zA-Z0-9']", '', word.lower()) in word_counts.keys():
-                    word_counts[re.sub("[^a-zA-Z0-9']", '', word.lower())] += number_appearances
-                else:
-                    word_counts[re.sub("[^a-zA-Z0-9']", '', word.lower())] = number_appearances
-    except IOError:
-        print('Error opening file')
-        continue
-	
-sorted_dict = sorted(((value, key) for (key,value) in word_counts.items()),reverse=True)
+update_json(sorted_results)
 
-try:
-    with open("cache.json",'w',encoding='UTF-8') as file:
-        json.dump(sorted_dict,file)
-except IOError:
-    pass
 end_time = time.time()
 
-print("%s seconds" % str(float(end_time)-float(start_time)))
+elapsed_time = end_time-start_time
+print("%s seconds" % str(elapsed_time))
